3398654de924418ddd9ad384d86799b6fa5e9dc6
Kabura Waruthi <kafurawaruthiq4@gmail.com>
2022-10-25 20:41:44 +0300

    chore: add algorithm

--- pascalstriangle.py
+++ pascalstriangle.py
@@ -52,16 +52,3 @@ for row in range(n):
         print(format(final[row][col], '<4'), end='')  # adds a spce after very number
 
     print()
-
-n = int(input('Number of Rows: '))
-for i in range(n + 1):
-    for j in range(n - i):
-        print(' ', end='')
-
-    C = 1
-    for j in range(1, i + 1):
-        print(C, ' ', sep='', end='')
-        C = C * (i - j) // j
-    print()
-
-
